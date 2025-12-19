dc43e74122c59fc86e1e180dff625ce0f98d113d
selencimen <selencimen7@gmail.com>
2025-12-19 14:27:06 +0300

    Add euclidean distance and complete all tests

--- utils/distances.py
+++ utils/distances.py
@@ -1,1 +1,26 @@
-pass  # YOUR CODE HERE
+def manhattan(a, b):
+    """
+    İki vektör arasındaki Manhattan mesafesini hesaplar.
+
+    Parametreler:
+    - a: liste veya tuple (ör. [1, 2])
+    - b: liste veya tuple (ör. [4, 6])
+
+    Dönüş:
+    - Manhattan mesafesi (int veya float)
+    """
+
+    distance = 0
+
+    for ai, bi in zip(a, b):
+        distance += abs(ai - bi)
+
+    return distance
+
+
+def euclidean(a, b):
+    dx = b[0] - a[0]
+    dy = b[1] - a[1]
+    return (dx**2 + dy**2) ** 0.5
+
+
